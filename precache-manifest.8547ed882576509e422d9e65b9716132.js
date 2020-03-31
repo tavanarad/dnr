e87bf47fb3b0aebbd21a66fd967a4e72d5f8a722
Morteza Tavanarad <tavanarad@gmail.com>
2020-04-01 04:41:26 +0800

    Updates

---
+++ precache-manifest.8547ed882576509e422d9e65b9716132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389cfc8834dc02b1b832322fc82609bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f90be8d22582966930eb",
+    "url": "/static/css/main.d927f850.chunk.css"
+  },
+  {
+    "revision": "53d70768d964a86d3758",
+    "url": "/static/js/2.a59ae0ee.chunk.js"
+  },
+  {
+    "revision": "45e78cf8d545cb6a9d6682654ac770f1",
+    "url": "/static/js/2.a59ae0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90be8d22582966930eb",
+    "url": "/static/js/main.498ce645.chunk.js"
+  },
+  {
+    "revision": "267ce1d2d3f4590e57fe",
+    "url": "/static/js/runtime-main.f73f5ef2.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
