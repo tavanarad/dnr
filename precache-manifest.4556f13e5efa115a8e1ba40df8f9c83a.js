80a9ba5b52ca86a1a8c8c685785e0cc19ea62c9b
Morteza Tavanarad <tavanarad@gmail.com>
2023-11-26 11:01:51 +0100

    build

---
+++ precache-manifest.4556f13e5efa115a8e1ba40df8f9c83a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9169ac369eec9175ed9e316291e057f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49e8257dadae747663d",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "fb883963243344b6655a",
+    "url": "/static/css/main.6a830a2f.chunk.css"
+  },
+  {
+    "revision": "e49e8257dadae747663d",
+    "url": "/static/js/2.dc08da86.chunk.js"
+  },
+  {
+    "revision": "b2e7c69d6b39b2f05dbe6610e23b7c81",
+    "url": "/static/js/2.dc08da86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb883963243344b6655a",
+    "url": "/static/js/main.e72d885d.chunk.js"
+  },
+  {
+    "revision": "267ce1d2d3f4590e57fe",
+    "url": "/static/js/runtime-main.f73f5ef2.js"
+  }
+]);
