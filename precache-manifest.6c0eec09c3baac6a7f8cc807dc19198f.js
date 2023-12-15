96548fbf76bca7fc4a3631077e84830ef4cc73d8
Morteza Tavanarad <tavanarad@gmail.com>
2023-12-15 13:50:29 +0100

    Updates

---
+++ precache-manifest.6c0eec09c3baac6a7f8cc807dc19198f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3431dc5d487075270512e98cf8daa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5778c4883ea319599eb",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "0c406aa2d5401f32acb6",
+    "url": "/static/css/main.6a830a2f.chunk.css"
+  },
+  {
+    "revision": "d5778c4883ea319599eb",
+    "url": "/static/js/2.087c5e6c.chunk.js"
+  },
+  {
+    "revision": "b2e7c69d6b39b2f05dbe6610e23b7c81",
+    "url": "/static/js/2.087c5e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c406aa2d5401f32acb6",
+    "url": "/static/js/main.6e962403.chunk.js"
+  },
+  {
+    "revision": "267ce1d2d3f4590e57fe",
+    "url": "/static/js/runtime-main.f73f5ef2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cfc0b1affdc4ea3912a1b581235d3a77.js"
+  "/precache-manifest.6c0eec09c3baac6a7f8cc807dc19198f.js"
 );
 
 self.addEventListener('message', (event) => {
